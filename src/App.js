6c70ca54764323df7b38d3ca2c35caac70e9f077
jinsong <56numghest@naver.com>
2023-01-07 18:39:44 +0900

    klaytn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { configureChains ,createClient,WagmiConfig  } from 'wagmi'
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+import { publicProvider } from 'wagmi/providers/public'
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
 
+import { Web3Modal } from "@web3modal/react";
+import './App.css';
 function App() {
+  const [WagmiC , setWagmiC] = useState(null)
+  useEffect(()=>{
+    const klaytn = {
+      "id": 8217,
+      "network": "Klaytn Mainnet",
+      "name": "Klaytn",
+      "nativeCurrency": {
+          "name": "klaytn",
+          "symbol": "klay",
+          "decimals": 18
+      },
+      "rpcUrls": {
+          default : {
+            "http": [
+                "https://public-node-api.klaytnapi.com/v1/cypress"
+            ]
+        }
+      },
+    }
+    const chainList = [arbitrum, mainnet, polygon]
+
+
+
+
+    },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>connect</button>
+      
+      
     </div>
   );
 }
